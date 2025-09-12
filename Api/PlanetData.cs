c855658f5cd81b4e1e47203a2b5676e5fe233f60
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-12 12:31:15 -0500

    created a planet reader and put it all in a dictionary :)

---
+++ Api/PlanetData.cs
@@ -0,0 +1,10 @@
+// public record PlanetData
+// {
+//   public Dictionary<string, List<Root>> Planets { get; init; }
+
+//   public PlanetData(Root planets)
+//   {
+//     foreach(var planet in planets)
+//     Planets[planet.name] = planet;
+//   }
+// };
--- App/App.cs
+++ App/App.cs
@@ -8,18 +8,54 @@ public class App
   {
     const string baseAddress = "https://swapi.info/api/";
     const string requestUri = "planets";
-
+    var confirmedPlanet = false;
     var json = await _apiInterface.Read(baseAddress, requestUri);
     var root = JsonSerializer.Deserialize<List<Root>>(json);
 
-    System.Console.WriteLine("Loading Galaxy's Edge Planets");
-    System.Console.WriteLine("******************************");
+    Dictionary<string, Root> planetsDirectory = new();
+
     foreach (var planet in root)
     {
-      System.Console.WriteLine($"Planet {planet.name}, Population: {planet.population}, Diameter: {planet.diameter}, Surface Water Percentage {planet.surface_water}.");
+      planetsDirectory[planet.name] = planet;
     }
-    System.Console.WriteLine();
-    System.Console.WriteLine("Which property would you like to see?");
-    var userInput = Console.ReadLine();
+
+    do
+    {
+      System.Console.WriteLine("Which planet would you like to get info on?");
+      var userInput = Console.ReadLine();
+      if (!planetsDirectory.ContainsKey(userInput))
+      {
+        System.Console.WriteLine("Invalid choice");
+      }
+      else
+      {
+
+        System.Console.WriteLine($"{planetsDirectory[userInput].name}: Population size of {planetsDirectory[userInput].population}, diameter of {planetsDirectory[userInput].diameter}, and surface water percentage of {planetsDirectory[userInput].surface_water}%.");
+        confirmedPlanet = true;
+      }
+    } while (confirmedPlanet == false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // System.Console.WriteLine("Loading Galaxy's Edge Planets");
+    // System.Console.WriteLine("******************************");
+    // foreach (var planet in root)
+    // {
+    //   System.Console.WriteLine($"Planet {planet.name}, Population: {planet.population}, Diameter: {planet.diameter}, Surface Water Percentage {planet.surface_water}.");
+    // }
+    // System.Console.WriteLine();
+    // System.Console.WriteLine("Which property would you like to see?");
+    // var userInput = Console.ReadLine();
   }
 }
